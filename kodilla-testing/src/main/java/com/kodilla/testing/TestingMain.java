c4e2d0f6f81acb116fa3d6ce56d73362dc92a2e3
Sylwia Krzymowska <s.krzymowska@gmail.com>
2021-11-19 13:15:44 +0100

    Calculator class with unit tests added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+import java.awt.*;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("Joe");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("Joe")) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator unit test
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(2, 5);
+        int substractResult = calculator.substract(5, 2);
+
+        if (addResult == 7) {
+            System.out.println("add() method test ok");
+        } else {
+            System.out.println("add() method - error");
+        }
+
+        if (substractResult == 3) {
+            System.out.println("substract() method test ok");
+        } else {
+            System.out.println("add() method - error");
+        }
     }
 }
