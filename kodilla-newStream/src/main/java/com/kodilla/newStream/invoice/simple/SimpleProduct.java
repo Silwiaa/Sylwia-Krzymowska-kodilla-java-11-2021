fa77081bb852ea17428e88c6f8a53cb7b2ad975b
Sylwia Krzymowska <s.krzymowska@gmail.com>
2021-11-28 16:37:04 +0100

    excesises from 7.3 module done

---
+++ kodilla-newStream/src/main/java/com/kodilla/newStream/invoice/simple/SimpleProduct.java
@@ -0,0 +1,29 @@
+package com.kodilla.newStream.invoice.simple;
+
+public class SimpleProduct {
+    private final String productName;
+    private final double productPrice;
+
+    public SimpleProduct(String productName, double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SimpleProduct that = (SimpleProduct) o;
+
+        return getProductName() != null ? getProductName().equals(that.getProductName()) : that.getProductName() == null;
+    }
+}
---
+++ kodilla-newStream/src/main/java/com/kodilla/newStream/sand/SandStorage.java
@@ -0,0 +1,7 @@
+package com.kodilla.newStream.sand;
+
+import java.math.BigDecimal;
+
+public interface SandStorage {
+    BigDecimal getSandBeansQuantity();
+}
