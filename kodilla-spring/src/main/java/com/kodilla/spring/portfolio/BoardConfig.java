e712dfcbe98fad29db8b52b119452c3869bc7cf6
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-01-02 18:19:42 +0100

    Exc 14.3 done

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,46 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BoardConfig {
+
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList taskList1;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList taskList2;
+
+    @Autowired
+    @Qualifier("doneList")
+    TaskList taskList3;
+
+    @Bean(name = "toDoList")
+    @Scope("prototype")
+    public TaskList getToDoList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "inProgressList")
+    @Scope("prototype")
+    public TaskList getInProgressList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "doneList")
+    @Scope("prototype")
+    public TaskList getDoneTasks() {
+        return new TaskList();
+    }
+
+    @Bean
+    public Board getBoard() {
+        return new Board(taskList1,taskList2, taskList3);
+    }
+}
