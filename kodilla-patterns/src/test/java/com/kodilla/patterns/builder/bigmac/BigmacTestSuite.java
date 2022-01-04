697e18ddaf7d14fd2e54986fba59316d51bc3076
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-01-04 16:58:01 +0100

    Exc 15.5 done

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import com.kodilla.patterns.builder.pizza.Pizza;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+    @Test
+    void testBigMacNew() {
+        //Given
+        BigMac bigMac = new BigMac.BigMacBuilder()
+                .bun("No sesame bun")
+                .burgers(2)
+                .sauce("Standard")
+                .ingredient("Shredded Lettuce")
+                .ingredient("American Cheese")
+                .ingredient("Pickle Slices")
+                .ingredient("Onions")
+                .build();
+        System.out.println(bigMac);
+        //When
+        int howManyIngredients = bigMac.getIngredients().size();
+
+        //Then
+        assertEquals(4, howManyIngredients);
+    }
+}
