418aec360b87973eddf480fa13cfdd22d517117a
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-03-05 15:31:41 +0100

    Exc 31.2 done

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -9,33 +9,29 @@ import org.openqa.selenium.support.ui.Select;
 public class FacebookTestingApp {
     public static final String XPATH_CREATEACCOUNT = "//div[contains(@class, \"_6ltg\")]/a";
     public static final String XPATH_ACCEPTCOOKIES = "//div[contains(@class, \"_9xo5\")]/button[2]";
-    public static final String XPATH_DAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
-    public static final String XPATH_MONTH = "//div[contains(@class, \"_5k_5\")]/span/span/select[2]";
-    public static final String XPATH_YEAR = "//div[contains(@class, \"_5k_5\")]/span/span/select[3]";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/");
+        Thread.sleep(1000);
 
         WebElement acceptAllCookiesButton = driver.findElement(By.xpath(XPATH_ACCEPTCOOKIES));
+        acceptAllCookiesButton.click();
+        Thread.sleep(1000);
+
         WebElement createAccountButton = driver.findElement(By.xpath(XPATH_CREATEACCOUNT));
-
-        if (acceptAllCookiesButton.isDisplayed()) {
-            acceptAllCookiesButton.click();
-        }
-
         createAccountButton.click();
+        Thread.sleep(2000);
 
-        WebElement day = driver.findElement(By.xpath(XPATH_DAY));
-        WebElement month = driver.findElement(By.xpath(XPATH_MONTH));
-        WebElement year = driver.findElement(By.xpath(XPATH_YEAR));
+        setupSelectOption("day", driver, 18);
+        setupSelectOption("month", driver, 8);
+        setupSelectOption("year", driver, 35);
+    }
 
-        Select selectDay = new Select(day);
-        Select selectMonth = new Select(month);
-        Select selectYear = new Select(year);
-
-        selectDay.selectByIndex(18);
-        selectMonth.selectByIndex(8);
-        selectYear.selectByIndex(35);
+    public static void setupSelectOption(String id, WebDriver driver, int value) {
+        String xPathSelect = "//*[@id=\"" + id + "\"]";
+        WebElement element = driver.findElement(By.xpath(xPathSelect));
+        Select select = new Select(element);
+        select.selectByIndex(value);
     }
 }
